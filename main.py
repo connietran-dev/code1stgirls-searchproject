5f1a21b4174369e5c3769b2bf19abc15eeda04db
Connie Tran <Connie@cwpuk-2d2ghw3r.home>
2024-01-24 20:06:22 +0000

    Initialise project with API response

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from pprint import pprint
+
+anime_id = input('What anime are you searching for today?: ')
+
+url = f'https://api.jikan.moe/v4/anime?q={anime_id}&sfw'
+
+response = requests.get(url)
+print(response)
+
+anime_response = response.json()
+pprint(anime_response)
